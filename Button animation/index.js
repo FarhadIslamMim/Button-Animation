8a552e6cfaa3a46ece8d116a06b81c68eda05057
FarhadIslamMim <76618171+FarhadIslamMim@users.noreply.github.com>
2021-08-18 23:49:21 +0600

    Add files via upload

---
+++ Button animation/index.js
@@ -0,0 +1,20 @@
+
+for (var i = 0; i < 3; i++) {
+    document.querySelectorAll(".button")[i].addEventListener("click", function () {
+
+        var text = this.innerHTML;
+        console.log(text);
+
+        AnimationPlay(text);
+
+    });
+}
+
+function AnimationPlay(text) {
+   var selectedbutton= document.querySelector("."+text);
+   selectedbutton.classList.add("animation");
+
+   setTimeout(function(){
+       selectedbutton.classList.remove("animation");
+   },250);
+}
